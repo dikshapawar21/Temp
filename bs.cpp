9a7ffa231965a5cde18b9056607e3ecc0d3d368d
Diksha Pawar <dikshapawar059@gmail.com>
2025-09-01 11:55:59 +0530

    First commit

---
+++ bs.cpp
@@ -0,0 +1,18 @@
+#include "func.cpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        cout << "Element found at index " << result << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
+}
